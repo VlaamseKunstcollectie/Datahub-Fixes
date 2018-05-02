96f6a4d30db1a65c7c5f914377d6d49b464bc86f
Matthias Vandermaesen <matthias@colada.be>
2018-05-02 14:39:40 +0200

    Changed: use authority-files repo instead of custom folder.

--- scripts/vocabularies.sh
+++ scripts/vocabularies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Generating Persistent URL vocabulary.";
-catmandu import CSV to DBI --data_source dbi:SQLite:/tmp/import.PIDS_ALL_UTF8.sqlite < ~/Workbench/datahubdump/PIDS_ALL_UTF8.csv
+catmandu import CSV to DBI --data_source dbi:SQLite:/tmp/import.PIDS_ALL_UTF8.sqlite < ~/workspace/authority-files/PIDS_ALL_UTF8.csv
 
 echo "Generating Creators vocabulary.";
 catmandu import CSV to DBI --data_source dbi:SQLite:/tmp/import.CREATORS_UTF8.sqlite < ~/workspace/authority-files/CREATORS_UTF8.csv
