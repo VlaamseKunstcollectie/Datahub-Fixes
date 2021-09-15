4373f92de8ca40585ea9c4c4bf03db77759fffd1
Hero Solutions <info@herosolutions.be>
2021-09-15 11:27:29 +0200

    Adds en/nl arthub.ini files, removes pushing of data at the end

--- scripts/arthub-index.sh
+++ scripts/arthub-index.sh
@@ -45,23 +45,12 @@ catmandu convert OAI --url "$e" --handler lido to JSON --fix "$l" > /tmp/bulk.js
 # Fetch XML raw data from the OAI endpoint and add it to the JSON blob.
 
 echo "Fetching XML"
-perl process.pl $e > /tmp/bulk_raw.json
+perl process.pl $e > "/tmp/bulk_raw_${l}.json"
 
-echo "]" >> /tmp/bulk_raw.json
+echo "]" >> "/tmp/bulk_raw_${l}.json"
 
 # Cleans out the dataset. All records without an 'id' field are purged.
 
-cp /tmp/bulk_raw.json /tmp/bulk_unfiltered.json
-catmandu convert JSON to JSON --fix "select all_match(id, '.*\S.*')" < /tmp/bulk_unfiltered.json > /tmp/bulk_raw.json
-rm /tmp/bulk_unfiltered.json
-
-# Remove the index
-
-# bundle exec rake jetty:stop
-# rm -r jetty/solr/blacklight-core/data/*
-# bundle exec rake jetty:start
-
-# Push data to the index
-
-echo "Pushing data"
-dhconveyor index -p ../pipelines/arthub.ini -v
+cp "/tmp/bulk_raw_${l}.json" "/tmp/bulk_unfiltered_${l}.json"
+catmandu convert JSON to JSON --fix "select all_match(id, '.*\S.*')" < "/tmp/bulk_unfiltered_${l}.json" > "/tmp/bulk_raw_${l}.json"
+rm "tmp/bulk_unfiltered_${l}.json"
