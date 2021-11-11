35fdd93926dde533aa4ed2c116ea1e11c68d8ac9
Hero Solutions <info@herosolutions.be>
2021-11-11 23:47:15 +0100

    Adds arthub-index.sh for IIIF arthub

---
+++ scripts/arthub-index-iiif.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $0 -e <string> -l <string>" 1>&2;
+	exit 1;
+}
+
+while getopts ":e:l:" o; do
+    case "${o}" in
+        e)
+            e="${OPTARG}"
+            ;;
+        l)
+            l="${OPTARG}"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${e}" -o -z "${l}" ]; then
+    usage
+fi
+
+lang="nl"
+
+if [ ! -f "${l}" -o ! -r "${l}" ]; then
+	if [[ "$l" =~ ^[a-z]{2,3}$ ]]; then
+		lang=$l;
+		l="../datahub-oai-to-iiif-blacklight-solr-${l}.fix"
+		if [ ! -f "${l}" -o ! -r "${l}" ]; then
+			echo "The fix file ${l} does not exist or is not readable."
+			exit 1
+		fi
+	else
+		echo "The fix file ${l} does not exist or is not readable."
+		exit 1
+    fi
+fi
+
+# Fetch data from the OAI endpoint
+
+echo "Fetching data"
+catmandu convert OAI --url "$e" --handler lido to JSON --fix "$l" > /tmp/bulk.json
+
+# Fetch XML raw data from the OAI endpoint and add it to the JSON blob.
+
+echo "Fetching XML"
+perl process.pl $e > "/tmp/bulk_raw_${lang}.json"
+
+echo "]" >> "/tmp/bulk_raw_${lang}.json"
+
+# Cleans out the dataset. All records without an 'id' field are purged.
+
+cp "/tmp/bulk_raw_${lang}.json" "/tmp/bulk_unfiltered_${lang}.json"
+catmandu convert JSON to JSON --fix "select all_match(id, '.*\S.*')" < "/tmp/bulk_unfiltered_${lang}.json" > "/tmp/bulk_raw_${lang}.json"
+rm "/tmp/bulk_unfiltered_${lang}.json"
